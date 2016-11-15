c8fdfec633a23c714641f1ac6831d07141d282cd
Carlos Alberto Pinedo García <cpinedogarcia@gmail.com>
2016-11-15 14:03:14 -0600

    Add project files.

---
+++ RecipeMPin/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\zeron\Documents\Visual Studio 2015\Projects\RecipeMPin\RecipeMPin\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
