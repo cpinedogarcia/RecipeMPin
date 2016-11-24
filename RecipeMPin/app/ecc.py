45c13cd2245b86dbbac3069e5cbe98807bd51eb6
Carlos Alberto Pinedo García <cpinedogarcia@gmail.com>
2016-11-24 14:46:57 -0600

    asdasdas

--- RecipeMPin/app/ecc.py
+++ RecipeMPin/app/ecc.py
@@ -1,7 +1,12 @@
 import hashlib
+from random import SystemRandom
 
 
 def hash_identity(identity):
     hid = hashlib.sha256()
     hid.update(identity.encode('utf-8'))
-    print(hid.hexdigest())
+    return hid.hexdigest()
+
+
+def generate_random(q):
+    return SystemRandom().randint(1, q)
