215e9f01c80e2ab645847fec639d6155a00833d2
JobWOD <job.jdrh@gmail.com>
2016-11-28 22:50:57 -0600

    Modelos

--- RecipeMPin/app/models.py
+++ RecipeMPin/app/models.py
@@ -4,4 +4,11 @@ Definition of models.
 
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    identity = models.CharField(max_length=40)
+    password = models.CharField(max_length=20)
+    pin = models.IntegerField()
+
+
